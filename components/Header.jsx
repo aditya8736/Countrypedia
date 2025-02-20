6f59362b87bc064de48f3f9827f4bc263a58c3ca
Aditya Kumar <adityrajput6342@gmail.com>
2025-02-20 12:11:17 +0530

    change logo and text

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,10 @@ export default function Header() {
     <header className={`header-container ${isDark? 'dark': ''}`}>
       <div className="header-content">
         <h2 className="title">
-          <a href="/">Where in the world?</a>
+          <a href="/">
+          <i class="fa-solid fa-globe"></i>
+          CountriesPedia
+          </a>
         </h2>
         <p className="theme-changer" onClick={() => {
           setIsDark(!isDark)
